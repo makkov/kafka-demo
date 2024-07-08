a83285dab356b3a5f7ab237c47fa364f168fc1ef
makkov <maxim_86nva@mail.ru>
2024-07-08 10:35:29 +0300

    init project

---
+++ src/main/java/com/example/colvir_kafka_demo/ColvirKafkaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.colvir_kafka_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColvirKafkaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColvirKafkaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/colvir_kafka_demo/ColvirKafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.colvir_kafka_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColvirKafkaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
