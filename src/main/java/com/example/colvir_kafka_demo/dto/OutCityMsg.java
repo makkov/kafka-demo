821773826ced1388f438bdeac98df8befac4681e
makkov <maxim_86nva@mail.ru>
2024-07-10 19:45:22 +0300

    отправка сообщения в фомрате json

---
+++ src/main/java/com/example/colvir_kafka_demo/dto/OutCityMsg.java
@@ -0,0 +1,14 @@
+package com.example.colvir_kafka_demo.dto;
+
+public class OutCityMsg {
+
+    private String city;
+
+    public OutCityMsg(String city) {
+        this.city = city;
+    }
+
+    public String getCity() {
+        return city;
+    }
+}
--- src/main/java/com/example/colvir_kafka_demo/kafka/KafkaProducer.java
+++ src/main/java/com/example/colvir_kafka_demo/kafka/KafkaProducer.java
@@ -1,12 +1,18 @@
 package com.example.colvir_kafka_demo.kafka;
 
+import com.example.colvir_kafka_demo.dto.OutCityMsg;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaProducer {
 
     private static final String CITY_OUT_BINDING_NAME = "city-out-0";
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     private final StreamBridge streamBridge;
 
@@ -15,6 +21,14 @@ public class KafkaProducer {
     }
 
     public void sendMsgToCityTopic(String city) {
-        streamBridge.send(CITY_OUT_BINDING_NAME, city);
+        Message<String> msg = null;
+        try {
+            msg = MessageBuilder
+                    .withPayload(objectMapper.writeValueAsString(new OutCityMsg(city)))
+                    .build();
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        streamBridge.send(CITY_OUT_BINDING_NAME, msg);
     }
 }
